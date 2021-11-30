b466128b1baceac5f48a9ad0c4b3cb9e81698f77
Nisarg000 <@nisargjhatakia94@gmail.com>
2021-11-30 17:07:33 +0530

    optimized Navbar

---
+++ js refresher/importmodule1.js
@@ -0,0 +1,6 @@
+// change the file name for both to .mjs to get the code working
+import ui from './module1.js';
+import {b,c,d} from './module1.js';
+// you can also write it as import ui, {b,c,d} from './module1.mjs';
+console.log(ui);
+console.log(c);
---
+++ js refresher/module1.js
@@ -0,0 +1,7 @@
+const a = "pure"
+const b= "donkey"
+const c= "Nisarg"
+const d= "Jhatakia"
+
+export default a;
+export {b,c,d};
