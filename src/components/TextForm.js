03c3212b6f12c93fd29256a03b8176817ed4e43f
Nisarg000 <@nisargjhatakia94@gmail.com>
2021-11-30 19:10:12 +0530

    added a lot of features in the textfform.js file like para,charcount,preview,min count,lowercase,changed navbar color,adjusted button space

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,22 +6,40 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+
+    const handleLoClick = () => {
+        // console.log("The uppercase button was used here" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     const handleOnChange = (event) => {
         // console.log("On change");
         setText(event.target.value); //this will enable us to type
     }
-    const [text, setText] = useState("Enter text here")//text is our variable and whenever text is updated then setText shall be called, learn this line by heart
+    const [text, setText] = useState("")//text is our variable and whenever text is updated then setText shall be called, learn this line by heart
 
     //in case if you want to change the text later in future then do this:
     //text = "replaced text" //this is the wrong method
     // setText("replaced text") //this is the correct method
     return (
-        <div>
+        <>
+        <div className="container">
             <h1>{props.heading} </h1>
             <div class="mb-3">
                 <textarea class="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick} >Convert to uppercase</button>
+            <button className="btn btn-primary mx-4" onClick={handleLoClick} >Convert to Lowercase</button>
+
         </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>{/* //count words and characters */}
+            <p> {0.008 * text.split(" ").length} Minutes required to read you texts</p> {/* Count minutes to read */}
+            <h2>Preview</h2> {/*  */}
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
