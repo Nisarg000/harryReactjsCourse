a255beee2d3a911040fc85885424085a5940fea6
Nisarg000 <@nisargjhatakia94@gmail.com>
2021-11-30 17:38:13 +0530

    I have added the concepts of props and modules(export default). props for resuing and their defaultprops and their types

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <>{/* In case if you want to use other elements than you will have to enclose it in fragments */}
       
-    <Navbar/>
+    <Navbar title = "TextUtils" aboutText="About TextUtils"/>
+    
+    {/* <Navbar /> {/* This calls the default type */}
+
+    {/* To use props first write the text in the component using props.xyzname[also write props in the default export function] and then use that propnamed variable here and use it */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+
+export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-        <a className="navbar-brand" href="/">TextUtils</a>
+        <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -14,7 +16,7 @@ export default function Navbar() {
               <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">About</a>
+              <a className="nav-link" href="/">{props.aboutText}</a>
             </li>
           </ul>
           <form className="d-flex">
@@ -25,4 +27,10 @@ export default function Navbar() {
       </div>
     </nav>
     )
+}
+
+Navbar.propTypes = {title: PropTypes.string.isRequired, aboutText: PropTypes.string.isRequired} // this is used to check the type of the props and would throw an error if the type is not correct 
+Navbar.defaultProps = {
+    title: "Set title here",
+    aboutText: "about text of default type"
 }
